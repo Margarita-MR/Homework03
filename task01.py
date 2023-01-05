b78cb8a18377e59b00c9c1f0570e669ee9237998
MargaritaM <ya.margarita1005@gmail.com>
2023-01-06 01:38:24 +0300

    Решена 1

---
+++ task01.py
@@ -0,0 +1,48 @@
+# Задайте список из нескольких чисел. Напишите программу, 
+# которая найдёт сумму элементов списка, стоящих на позиции с нечетным индексом.
+# Пример:
+# [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+# import operator
+# import math
+# from random import randrange
+# from xml.etree import ElementInclude
+# n = int(input('Введите максимальное число = '))
+# i = int(input('Введите количество чисел = '))
+# a = list(randrange(1, n) for i in range(i))
+# print(a)
+
+# A = list(a[1:i:2])
+# print(A)
+# S = sum(A)
+# print(S)
+
+
+
+
+import operator
+import math
+from random import randrange
+from xml.etree import ElementInclude
+n = int(input('Введите максимальное число = '))
+i = int(input('Введите количество чисел = '))
+a = list(randrange(1, n) for i in range(i))
+print(a)
+
+A = list(a[1:i:2])
+print(A)
+B = list(reversed(A))
+# print(B)
+
+Mul = list(map(operator.mul, A, B))
+print(Mul)
+
+middle = float(len(Mul))/2
+if middle % 2 != 0:
+    x = int(Mul[int(middle - .5)])
+else:
+    x = int(Mul[int(middle)], Mul[int(middle-1)])
+# print(x)
+k = Mul.index (x)
+Res = list(Mul[0:k+1:1])
+print(Res)
