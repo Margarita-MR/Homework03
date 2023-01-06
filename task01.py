9968c2dbdb4b1e2fc3d56a005c4ab199576f331d
MargaritaM <ya.margarita1005@gmail.com>
2023-01-06 16:52:04 +0300

    Решена 4

--- task01.py
+++ task01.py
@@ -3,6 +3,23 @@
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
+import operator
+import math
+from random import randrange
+from xml.etree import ElementInclude
+n = int(input('Введите максимальное число = '))
+i = int(input('Введите количество чисел = '))
+a = list(randrange(1, n) for i in range(i))
+print('Список чисел : ', a)
+
+A = list(a[1:i:2])
+print('Числа с нечетными индексами : ', A)
+S = sum(A)
+print('Сумма чисел = ', S)
+
+
+
+
 # import operator
 # import math
 # from random import randrange
@@ -14,35 +31,18 @@
 
 # A = list(a[1:i:2])
 # print(A)
-# S = sum(A)
-# print(S)
+# B = list(reversed(A))
+# # print(B)
 
+# Mul = list(map(operator.mul, A, B))
+# print(Mul)
 
-
-
-import operator
-import math
-from random import randrange
-from xml.etree import ElementInclude
-n = int(input('Введите максимальное число = '))
-i = int(input('Введите количество чисел = '))
-a = list(randrange(1, n) for i in range(i))
-print(a)
-
-A = list(a[1:i:2])
-print(A)
-B = list(reversed(A))
-# print(B)
-
-Mul = list(map(operator.mul, A, B))
-print(Mul)
-
-middle = float(len(Mul))/2
-if middle % 2 != 0:
-    x = int(Mul[int(middle - .5)])
-else:
-    x = int(Mul[int(middle)], Mul[int(middle-1)])
-# print(x)
-k = Mul.index (x)
-Res = list(Mul[0:k+1:1])
-print(Res)
+# middle = float(len(Mul))/2
+# if middle % 2 != 0:
+#     x = int(Mul[int(middle - .5)])
+# else:
+#     x = int(Mul[int(middle)], Mul[int(middle-1)])
+# # print(x)
+# k = Mul.index (x)
+# Res = list(Mul[0:k+1:1])
+# print(Res)
---
+++ task03.py
@@ -0,0 +1,36 @@
+# Задайте список из вещественных чисел. 
+# Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов, 
+# отличной от 0.
+# Пример:
+# [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+from fractions import Fraction
+import math
+import decimal
+from decimal import Decimal
+
+n = int(input('Количество вещественных чисел = '))
+import random
+x=list(round(random.uniform(0, 10), 2) for x in range(n))
+
+print(x)
+x = str(x)
+
+x=(x.replace('.',', '))
+
+# print(type(x))
+x = x.split(',')
+# print(x)
+# print(type(x))
+
+A = list(x[1:len(x):2])
+# print(A)
+
+
+Max = max(A)
+print('Максимальное значение дробной части : ', Max)
+Min = min(A)
+print('Минимальное значение дробной части : ', Min)
+print('Разность = ', int(Max)-int(Min))
+
+
+
